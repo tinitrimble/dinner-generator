a6784ccb661c985b599e5dcb9980c32d260c8563
Tini Trimble <tini.trimble@gmail.com>
2019-07-25 16:51:48 -0700

    set up json file for restaurant info and commented out start of graphql for index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from "react"
+// import { graphql } from "gatsby"
+
+// const Dinnergenerator = ({ data }) => {
+  // const restaurants = data.allDataJson.edges.map(edge => edge.node);
+  // return (
+    // <div className="Generator">
+
+    // </div>
+  // )
+// }
+
+// export const query = graphql`
+  // query GetRestaurantQuery {
+    // allDataJson {
+      // edges {
+        // node {
+         // restaurants {
+           // location
+           // sampleitems
+           // notables
+         // }
+       // }
+     // }
+   // }
+ // }
+// `
 
 export default () => <div>Hello world!</div>
